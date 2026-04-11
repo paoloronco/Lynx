0fa3dc8d585b3aa932beba9042a76a0321eef521
Paolo <paolo.ronco2000@gmail.com>
2026-04-11 14:51:42 +0200

    Fix missing fs import in database.js causing Docker runtime error

--- LYNX/server/database.js
+++ LYNX/server/database.js
@@ -1,6 +1,7 @@
 import sqlite3 from 'sqlite3';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
