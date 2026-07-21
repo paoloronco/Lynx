5636897751426b092abd77b925d32e3412092d8c
Paolo <paolo.ronco2000@gmail.com>
2026-07-21 18:06:06 +0200

    fix: refine mobile dashboard layout

--- install-pve.sh
+++ install-pve.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
-readonly SCRIPT_VERSION="4.11.4"
+readonly SCRIPT_VERSION="4.11.5"
 readonly GUEST_INSTALLER_URL="https://raw.githubusercontent.com/paoloronco/OrbitPage/main/install.sh"
 
 CTID="${ORBITPAGE_PVE_CTID:-}"
